074a29c2d6dbd174d97d732284afb3417566b940
Roberto Gomez <rgomezmartearena@ubp.edu.ar>
2024-10-13 03:41:05 -0300

    Se modifico el tipo de dato para figures

--- figurasv2/src/main/java/ar/edu/ubp/das/appref/ShapePanel.java
+++ figurasv2/src/main/java/ar/edu/ubp/das/appref/ShapePanel.java
@@ -3,7 +3,8 @@ package ar.edu.ubp.das.appref;
 import javax.swing.JPanel;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Rectangle2D;
 import java.awt.Color;
@@ -16,20 +17,20 @@ class ShapePanel extends JPanel {
     public static final double DEFAULT_SIZE = 100.0;
     public static final double SIZE_INCREMENT = 20.0;
 
-    private LinkedList<Figure> figures;
+    private List<Figure> figures;
 
     public ShapePanel() {
-        this.figures = new LinkedList<Figure>();
-        this.figures.add(new Figure(new Rectangle2D.Double(50, 50, DEFAULT_SIZE, DEFAULT_SIZE), Color.RED, DEFAULT_SIZE));
-        this.figures.add(new Figure(new Ellipse2D.Double(200, 50, DEFAULT_SIZE, DEFAULT_SIZE), Color.GREEN, DEFAULT_SIZE));
-        this.figures.add(new Figure(new Rectangle2D.Double(350, 50, DEFAULT_SIZE, DEFAULT_SIZE), Color.BLUE, DEFAULT_SIZE));
+        this.figures = new ArrayList<>();
+        figures.add(new Figure(new Rectangle2D.Double(50, 50, DEFAULT_SIZE, DEFAULT_SIZE), Color.RED, DEFAULT_SIZE));
+        figures.add(new Figure(new Ellipse2D.Double(200, 50, DEFAULT_SIZE, DEFAULT_SIZE), Color.GREEN, DEFAULT_SIZE));
+        figures.add(new Figure(new Rectangle2D.Double(350, 50, DEFAULT_SIZE, DEFAULT_SIZE), Color.BLUE, DEFAULT_SIZE));
 
         addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                for (int i = 0; i < figures.size(); i++) {
-                    if (figures.get(i).getShape().contains(e.getPoint())) {
-                        figures.get(i).setSize(figures.get(i).getSize() + SIZE_INCREMENT);
+                for (Figure figure : figures) {
+                    if (figure.getShape().contains(e.getPoint())) {
+                        figure.setSize(figure.getSize() + SIZE_INCREMENT);
                     }
                 }
                 repaint();
